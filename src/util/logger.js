b2d5953ac166e559b2c26d0c9340c8b69d4171a0
Canale <raffaelecanale@gmail.com>
2018-03-03 19:45:55 +0100

    Logger has own level

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,20 +1,21 @@
 import chalk from 'chalk';
 import { getOrExec, prettyPrint } from 'util/utils';
 
-function levelFormatter(color, level) {
-    return color(level.substring(0, 4).toUpperCase());
-}
-
 const defaultOptions = {
     transport: console,
-    levels: {
-        verbose: levelFormatter.bind(null, chalk.cyan),
-        info: levelFormatter.bind(null, chalk.green),
-        warning: levelFormatter.bind(null, chalk.yellow),
-        error: levelFormatter.bind(null, chalk.red),
+    levels: [
+        'verbose',
+        'info',
+        'warn',
+        'error',
+    ],
+    messageFormatter: info => `${info.timestamp} ${info.level} [${info.name}] - ${info.message}`,
+    levelFormatter: (level, index) => {
+        const color = [chalk.cyan, chalk.green, chalk.yellow, chalk.red][index] || chalk.white;
+        return color(level.substring(0, 4).toUpperCase());
     },
-    formatter: info => `${info.timestamp} ${info.level} [${info.name}] - ${info.message}`,
     nameFormatter: chalk.bold,
+    level: 'verbose',
 };
 
 class Logger {
@@ -22,20 +23,35 @@ class Logger {
     constructor(name, options) {
         this.name = name;
         _.assign(this, _.defaults(options, defaultOptions));
+        this.levelIndex = this._getLevelIndex(this.level);
 
-        _.forEach(this.levels, (formatter, level) => {
-            this[level] = this.log.bind(this, formatter(level));
+        this.levels.forEach((level) => {
+            this[level] = this.log.bind(this, level);
         });
     }
 
     log(level, ...message) {
+        const levelIndex = this._getLevelIndex(level);
+        if (levelIndex < this.levelIndex) {
+            return;
+        }
+
         const info = {
             timestamp: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
             name: this.nameFormatter(getOrExec(this.name)),
-            level,
+            level: this.levelFormatter(level, levelIndex),
             message: message.map(prettyPrint).join(' '),
         };
-        this.transport.log(this.formatter(info));
+        this.transport.log(this.messageFormatter(info));
+    }
+
+    _getLevelIndex(level) {
+        const index = _.indexOf(this.levels, level);
+        if (index < 0) {
+            throw new Error(`Level ${level} must be in ${this.levels}`);
+        }
+
+        return index;
     }
 }
 
